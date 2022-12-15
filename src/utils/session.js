c3de8c38bf9290ae80bd57049f0505788d39fa4a
VoVaStudios <mateuszscieginskki@gmail.com>
2022-12-15 15:01:41 +0100

    init

---
+++ src/utils/session.js
@@ -0,0 +1,13 @@
+export const setUserSession = (token, user) => {
+    localStorage.setItem('token', token)
+  }
+  
+  
+  
+  export const removeUserSession = () => {
+    localStorage.removeItem('token');
+  }
+  export const getToken = () => {
+    if(localStorage.getItem('token') !== null) return true;
+    return false;
+  }
